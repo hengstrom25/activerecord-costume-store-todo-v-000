bc084d87d1fd9da8eaf96ce4f408ffb3a10dca5e
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-22 16:50:43 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,6 +8,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
+      t.date :opening_date
+      t.date :closing_date
+      t.string :description
     end
   end
 end
