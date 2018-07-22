56c49429b5931357463c352be32f27868c4dcb1d
Heidi Engstrom <hengstrom25@gmail.com>
2018-07-22 16:42:17 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,6 +9,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       t.integer :num_of_employees
       t.boolean :still_in_business
       t.timestamp :opening_time
+      t.timestamp :closing_time
     end
   end
 end
